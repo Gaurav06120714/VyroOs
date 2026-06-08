9b124e2738bc43a14d458404423891d3de364990
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-08 23:31:21 +0530

    Updated version

--- kernel/widgets_panel.h
+++ kernel/widgets_panel.h
@@ -3,8 +3,6 @@
 
 #include "../include/types.h"
 
-// Renders the macOS-Sequoia-style widget stack on the right side
-// of the desktop. Each widget is a glassmorphic-looking card.
 void widgets_panel_draw(uint32_t panel_x, uint32_t panel_y, uint32_t panel_w);
 
 #endif
