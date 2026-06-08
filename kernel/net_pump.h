3ab474bfae36fad99ed4d59c932c6a5dea96ed10
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-08 23:29:18 +0530

    Updated version

--- kernel/net_pump.h
+++ kernel/net_pump.h
@@ -3,9 +3,6 @@
 
 #include "../include/types.h"
 
-// Drain the RX queue and dispatch frames through ARP and UDP for `ms` milliseconds.
-// Designed to be called from the main thread / shell commands that wait for replies.
-// Uses `hlt` between polls so idle CPU is near zero.
 void net_pump_run(uint32_t ms);
 
 #endif
