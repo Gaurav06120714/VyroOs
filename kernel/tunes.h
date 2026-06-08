70d5bfb6eb25aed6a71e2745f9a78be80f8cc069
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-08 23:30:53 +0530

    Updated version

--- kernel/tunes.h
+++ kernel/tunes.h
@@ -3,13 +3,10 @@
 
 #include "../include/types.h"
 
-// Built-in PC-speaker tunes. Each tune is a sequence of (note frequency Hz,
-// duration ms) pairs played back via the speaker.h API.
-
-void tunes_play_boot(void);     // Boot chime
-void tunes_play_alert(void);    // Short alert
-void tunes_play_scale(void);    // C major scale demo
-void tunes_play_arpeggio(void); // C-E-G-C ascending
-void tunes_play_error(void);    // Low warble
+void tunes_play_boot(void);
+void tunes_play_alert(void);
+void tunes_play_scale(void);
+void tunes_play_arpeggio(void);
+void tunes_play_error(void);
 
 #endif
