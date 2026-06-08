152179a693fe915a483c750d76a4f04636ad594f
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-08 23:28:10 +0530

    Updated version

--- kernel/gdt.h
+++ kernel/gdt.h
@@ -3,12 +3,11 @@
 
 #include "../include/types.h"
 
-// Segment selectors (index << 3 | RPL)
-#define SEL_KCODE  0x08          // Ring 0 code
-#define SEL_KDATA  0x10          // Ring 0 data
-#define SEL_UCODE  (0x18 | 3)    // Ring 3 code = 0x1B
-#define SEL_UDATA  (0x20 | 3)    // Ring 3 data = 0x23
-#define SEL_TSS    0x28          // TSS descriptor
+#define SEL_KCODE  0x08
+#define SEL_KDATA  0x10
+#define SEL_UCODE  (0x18 | 3)
+#define SEL_UDATA  (0x20 | 3)
+#define SEL_TSS    0x28
 
 void gdt_init();
 void tss_set_rsp0(uint64_t rsp0);
