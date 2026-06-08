4a88fd3f4a180511d6fb73930e83e20c30d1fe69
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-08 23:27:41 +0530

    Updated version

--- kernel/dhcp_real.h
+++ kernel/dhcp_real.h
@@ -3,8 +3,6 @@
 
 #include "../include/types.h"
 
-// Returns 1 if a real DHCP exchange succeeded within `timeout_ms`.
-// On success, the OS's IP / gateway / DNS are updated from the OFFER.
 int dhcp_real_run(uint32_t timeout_ms);
 
 const uint8_t* dhcp_real_ip();
