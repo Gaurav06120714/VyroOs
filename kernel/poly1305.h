f0daf766821fd819824a72dcd6c5b804d488984f
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-08 23:29:43 +0530

    Updated version

--- kernel/poly1305.h
+++ kernel/poly1305.h
@@ -3,9 +3,6 @@
 
 #include "../include/types.h"
 
-// Poly1305 one-shot MAC per RFC 8439 §2.5.
-// key: 32 bytes (16 r + 16 s). out: 16-byte authentication tag.
-
 void poly1305_mac(uint8_t out[16], const uint8_t* msg, uint32_t len,
                   const uint8_t key[32]);
 
