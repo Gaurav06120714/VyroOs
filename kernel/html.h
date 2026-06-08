18908d201d8b138bfecec51444f33e787f9b94e9
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-08 23:28:28 +0530

    Updated version

--- kernel/html.h
+++ kernel/html.h
@@ -1,9 +1,6 @@
 #ifndef HTML_H
 #define HTML_H
 
-// Render a minimal HTML string onto the framebuffer.
-// Supports: <h1> <h2> <p> <b> <a> <ul> <li> <br> and text.
-// Returns when the user presses ESC.
 void html_render(const char* html);
 
 #endif
