1f72647f1385f48b70c00c1729d58c0b5c423298
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-08 23:27:19 +0530

    Updated version

--- kernel/chacha20.h
+++ kernel/chacha20.h
@@ -3,9 +3,6 @@
 
 #include "../include/types.h"
 
-// ChaCha20 stream cipher per RFC 8439 §2.4.
-// key: 32 bytes (little-endian). nonce: 12 bytes. counter: starting block number.
-
 void chacha20_block(const uint8_t key[32], uint32_t counter,
                     const uint8_t nonce[12], uint8_t out[64]);
 
