ea7f02590727eef2bb668f70244faff4cb791edc
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-08 23:30:35 +0530

    Updated version

--- kernel/task.h
+++ kernel/task.h
@@ -3,29 +3,28 @@
 
 #include "../include/types.h"
 
-#define TASK_STACK_SIZE  8192    // 8KB stack per task
+#define TASK_STACK_SIZE  8192
 #define TASK_NAME_MAX    32
 #define MAX_TASKS        16
 
-// Task states
 #define TASK_READY       0
 #define TASK_RUNNING     1
 #define TASK_FINISHED    2
 
 typedef struct task {
-    uint64_t      rsp;                  // Saved stack pointer (MUST be first field)
+    uint64_t      rsp;
     uint64_t      id;
     char          name[TASK_NAME_MAX];
     uint8_t       state;
-    struct task*  next;                 // Circular linked list
-    uint8_t*      stack_base;           // Allocated stack (for freeing)
+    struct task*  next;
+    uint8_t*      stack_base;
 } task_t;
 
 void   tasking_init();
 task_t* task_create(const char* name, void (*entry)(void));
 void   task_yield();
 void   task_exit();
-void   task_run_all();                  // Run scheduler until all tasks finish
+void   task_run_all();
 
 uint32_t task_count();
 task_t*  task_list_head();
